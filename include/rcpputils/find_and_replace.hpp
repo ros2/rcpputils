5d26dbff5c48c7e7ae945a2a521e25a4333e5a66
Jacob Perron <jacob@openrobotics.org>
2019-06-07 17:10:33 -0700

    Add function 'find_and_replace'
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

---
+++ include/rcpputils/find_and_replace.hpp
@@ -0,0 +1,50 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RCPPUTILS__FIND_AND_REPLACE_HPP_
+#define RCPPUTILS__FIND_AND_REPLACE_HPP_
+
+#include <string>
+
+namespace rcpputils
+{
+
+/// Find and replace all instances of a string with another string.
+/**
+ * \param[in] input The input string.
+ * \param[in] find The substring to replace.
+ * \param[in] replace The string to substitute for each occurance of `find`.
+ * \return A copy of the input string with all instances of the string `find` replaced with the
+ *   string `replace`.
+ */
+inline std::string
+find_and_replace(const std::string & input, const std::string & find, const std::string & replace)
+{
+  std::string output = input;
+  const std::size_t find_len = find.length();
+  const std::size_t replace_len = replace.length();
+  if (0u == find_len) {
+    return output;
+  }
+  std::size_t pos = output.find(find);
+  while (pos != std::string::npos) {
+    output.replace(pos, find_len, replace);
+    pos = output.find(find, pos + replace_len);
+  }
+  return output;
+}
+
+}  // namespace rcpputils
+
+#endif  // RCPPUTILS__FIND_AND_REPLACE_HPP_
---
+++ test/test_find_and_replace.cpp
@@ -0,0 +1,70 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include "rcpputils/find_and_replace.hpp"
+
+TEST(test_find_and_replace, find_and_replace) {
+  // Empty input
+  {
+    auto ret = rcpputils::find_and_replace("", "foo", "bar");
+    EXPECT_EQ("", ret);
+  }
+  // Empty find
+  {
+    auto ret = rcpputils::find_and_replace("foo", "", "bar");
+    EXPECT_EQ("foo", ret);
+  }
+  // Empty replace
+  {
+    auto ret = rcpputils::find_and_replace("foo", "foo", "");
+    EXPECT_EQ("", ret);
+  }
+  // Single occurance
+  {
+    auto ret = rcpputils::find_and_replace("foo", "foo", "bar");
+    EXPECT_EQ("bar", ret);
+  }
+  // No occurances
+  {
+    auto ret = rcpputils::find_and_replace("foo", "bar", "baz");
+    EXPECT_EQ("foo", ret);
+  }
+  // Multiple occurances
+  {
+    auto ret = rcpputils::find_and_replace("foobarfoobar", "foo", "baz");
+    EXPECT_EQ("bazbarbazbar", ret);
+  }
+  {
+    auto ret = rcpputils::find_and_replace("foofoobar", "foo", "baz");
+    EXPECT_EQ("bazbazbar", ret);
+  }
+  // find == replace
+  {
+    auto ret = rcpputils::find_and_replace("foobar", "foo", "foo");
+    EXPECT_EQ("foobar", ret);
+  }
+  // find is a substring of replace
+  {
+    auto ret = rcpputils::find_and_replace("foobar", "foo", "barfoo");
+    EXPECT_EQ("barfoobar", ret);
+  }
+  // replace is a substring of find
+  {
+    auto ret = rcpputils::find_and_replace("foobar", "foobar", "bar");
+    EXPECT_EQ("bar", ret);
+  }
+}
