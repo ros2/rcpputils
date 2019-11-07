16f07c9921c187bc13c1af8a7aa6fe5726a0a9fb
Anas Abou Allaban <allabana@amazon.com>
2019-11-07 07:58:25 -0800

    Add new copy constructor and address PR comments
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>

--- include/rcpputils/filesystem_helper.hpp
+++ include/rcpputils/filesystem_helper.hpp
@@ -91,6 +91,8 @@ public:
     std::replace(path_.begin(), path_.end(), '/', kPreferredSeparator);
   }
 
+  path(const path & p) = default;
+
   std::string string() const
   {
     return path_;
@@ -205,13 +207,14 @@ inline bool create_directories(const path & p)
  */
 path remove_extension(const path & file_path, int n_times = 1)
 {
-  path new_path = path(file_path);
+  path new_path(file_path);
   for (int i = 0; i < n_times; i++) {
-    size_t last_dot = new_path.string().find_last_of('.');
+    const auto new_path_str = new_path.string();
+    const auto last_dot = new_path_str.find_last_of('.');
     if (last_dot == std::string::npos) {
       return new_path;
     }
-    new_path = path(new_path.string().substr(0, last_dot));
+    new_path = path(new_path_str.substr(0, last_dot));
   }
   return new_path;
 }
