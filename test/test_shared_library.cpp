b19a01d549aae753a414441ec911d2b0fc293fa3
ahcorde <ahcorde@gmail.com>
2020-03-23 09:40:27 +0100

    Added basic test with sanitizers for shared libray
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ test/test_shared_library.cpp
@@ -0,0 +1,36 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "rcpputils/shared_library.hpp"
+
+TEST(test_shared_library, valid_load) {
+  // library path
+  const std::string library_path = std::string("libdummy_shared_library.so");
+
+  EXPECT_NO_THROW(std::make_shared<rcpputils::SharedLibrary>(library_path));
+  EXPECT_ANY_THROW(std::make_shared<rcpputils::SharedLibrary>("library_path"));
+
+  auto library = std::make_shared<rcpputils::SharedLibrary>(library_path);
+
+  EXPECT_TRUE(library->has_symbol("print_name"));
+  EXPECT_FALSE(library->has_symbol("symbol"));
+
+  EXPECT_TRUE(library->get_symbol("print_name") != NULL);
+  EXPECT_ANY_THROW(library->get_symbol("symbol"));
+
+}
