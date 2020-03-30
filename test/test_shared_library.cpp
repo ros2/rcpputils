d8000c427a56bb51a7d66fd8e5d29112ce3e32e3
ahcorde <ahcorde@gmail.com>
2020-03-30 10:08:13 +0200

    Fixed cpplint test_shared_library
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- test/test_shared_library.cpp
+++ test/test_shared_library.cpp
@@ -33,7 +33,6 @@ TEST(test_shared_library, valid_load) {
 }
 
 TEST(test_shared_library, failed_test) {
-
   // loading a library that doesn't exists
   std::string library_path = std::string("error_library.so");
   EXPECT_THROW(std::make_shared<rcpputils::SharedLibrary>(library_path), std::runtime_error);
