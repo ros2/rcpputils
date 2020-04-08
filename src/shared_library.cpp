e77cc8471765407bfb93b950c637353120182e33
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2020-04-08 23:51:34 +0200

    Added debug version for library names (#52)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/shared_library.cpp
+++ src/shared_library.cpp
@@ -85,13 +85,14 @@ std::string SharedLibrary::get_library_path()
   throw std::runtime_error{"Library path is not defined"};
 }
 
-std::string get_platform_library_name(std::string library_name)
+std::string get_platform_library_name(std::string library_name, bool debug)
 {
   char library_name_platform[1024];
   rcutils_ret_t ret = rcutils_get_platform_library_name(
     library_name.c_str(),
     library_name_platform,
-    1024);
+    1024,
+    debug);
   if (ret != RCUTILS_RET_OK) {
     std::string rcutils_error_str(rcutils_get_error_string().str);
     rcutils_reset_error();
