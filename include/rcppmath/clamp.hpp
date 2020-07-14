a227baca62fe05481e56c65d4fe66909968eb022
Victor Lopez <victor.lopez@pal-robotics.com>
2020-07-14 08:07:01 +0200

    Extend documentation of clamp methods
    Signed-off-by: Victor Lopez <victor.lopez@pal-robotics.com>

--- include/rcppmath/clamp.hpp
+++ include/rcppmath/clamp.hpp
@@ -23,10 +23,18 @@
 
 namespace rcppmath
 {
-
-// Implementation from https://en.cppreference.com/w/cpp/algorithm/clamp
-// @note Capturing the result of clamp by reference if one of the parameters
-//       is rvalue produces a dangling reference if that parameter is returned
+/// If v compares less than lo, returns lo; otherwise if hi compares less
+//  than v, returns hi; otherwise returns v. Uses operator< to compare the values
+/**
+ * \param[in] v the value to clamp
+ * \param[in] lo the lower boundary
+ * \param[in] hi the higher boundary
+ * \return Reference to lo if v is less than lo, reference to hi if hi is less than v, otherwise
+ * reference to v.
+ * \note Implementation from https://en.cppreference.com/w/cpp/algorithm/clamp
+ * \warning Capturing the result of clamp by reference if one of the parameters is rvalue produces
+ *  a dangling reference if that parameter is returned
+ **/
 template<class T>
 constexpr const T & clamp(const T & v, const T & lo, const T & hi)
 {
@@ -34,6 +42,11 @@ constexpr const T & clamp(const T & v, const T & lo, const T & hi)
   return (v < lo) ? lo : (hi < v) ? hi : v;
 }
 
+/// Like the function above, but uses comp to compare the values.
+/**
+ * \param[in] comp Comparison object that returns true if the first argument is
+ * less than the second
+ **/
 template<class T, class Compare>
 constexpr const T & clamp(const T & v, const T & lo, const T & hi, Compare comp)
 {
