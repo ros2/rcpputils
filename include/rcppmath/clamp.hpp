be1b683a20695ec31f4a9c9437a5a6cdbbb3e018
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-07-20 17:08:01 -0400

    Improve `rcppmath` Doxygen documentation (#138)
    This PR broadly attempts to make `rcppmath` documentation more detailed, as well as fixes some Doxygen issues that previously prevented `rcppmath` documentation from building correctly using `rosdoc2`.
    
    Specifically:
    * Documented rolling mean accumulator methods.
    * For the clamping methods:
      * Added \sa tag instead of explicitly asking user to "see the other function".
      * nit: Capitalization.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>
    
    Co-authored-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/rcppmath/clamp.hpp
+++ include/rcppmath/clamp.hpp
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /*! \file clamp.hpp
- * \brief Restrict a value between two bounds
+ *  \brief Restrict a value between two bounds.
  */
 
 #ifndef RCPPMATH__CLAMP_HPP_
@@ -23,18 +23,19 @@
 
 namespace rcppmath
 {
-/// If v compares less than lo, returns lo; otherwise if hi compares less
-//  than v, returns hi; otherwise returns v. Uses operator< to compare the values
 /**
- * \param[in] v the value to clamp
- * \param[in] lo the lower boundary
- * \param[in] hi the higher boundary
+ * If v compares less than lo, returns lo; otherwise if hi compares less
+ * than v, returns hi; otherwise returns v. Uses operator< to compare the values.
+ *
+ * \param[in] v The value to clamp.
+ * \param[in] lo The lower boundary.
+ * \param[in] hi The higher boundary.
  * \return Reference to lo if v is less than lo, reference to hi if hi is less than v, otherwise
  * reference to v.
- * \note Implementation from https://en.cppreference.com/w/cpp/algorithm/clamp
+ * \note Implementation from https://en.cppreference.com/w/cpp/algorithm/clamp.
  * \warning Capturing the result of clamp by reference if one of the parameters is rvalue produces
- *  a dangling reference if that parameter is returned
- **/
+ *  a dangling reference if that parameter is returned.
+ */
 template<class T>
 constexpr const T & clamp(const T & v, const T & lo, const T & hi)
 {
@@ -42,11 +43,18 @@ constexpr const T & clamp(const T & v, const T & lo, const T & hi)
   return (v < lo) ? lo : (hi < v) ? hi : v;
 }
 
-/// Like the function above, but uses comp to compare the values.
 /**
+ * Performs clamping with a provided Comparison object (comp).
+ *
+ * \param[in] v The value to clamp.
+ * \param[in] lo The lower boundary.
+ * \param[in] hi The higher boundary.
  * \param[in] comp Comparison object that returns true if the first argument is
- * less than the second
- **/
+ * less than the second.
+ * \return Reference to lo if v is less than lo, reference to hi if hi is less than v, otherwise
+ * reference to v.
+ * \sa rcppmath::clamp(const T&, const T&, const T&)
+ */
 template<class T, class Compare>
 constexpr const T & clamp(const T & v, const T & lo, const T & hi, Compare comp)
 {
