ea935abd9c5d818c12a044b3d89de2ce97e11de7
Adam Aposhian <adam.aposhian@fireflyautomatix.com>
2025-06-18 07:01:38 -0600

    uncrustify and cpplint fixes
    Signed-off-by: Adam Aposhian <adam.aposhian@fireflyautomatix.com>

--- src/thread_name.cpp
+++ src/thread_name.cpp
@@ -34,18 +34,18 @@ void set_thread_name_windows(const std::string & name)
   int wchars_num = MultiByteToWideChar(CP_UTF8, 0, name.c_str(), -1, nullptr, 0);
   bool conversion_failed = wchars_num == 0;
   if (!conversion_failed) {
-      std::vector<wchar_t> wstr(wchars_num);
-      auto ret = MultiByteToWideChar(CP_UTF8, 0, name.c_str(), -1, wstr.data(), wchars_num);
-      bool conversion_failed = ret == 0;
-      if (!conversion_failed) {
+    std::vector<wchar_t> wstr(wchars_num);
+    auto ret = MultiByteToWideChar(CP_UTF8, 0, name.c_str(), -1, wstr.data(), wchars_num);
+    bool conversion_failed = ret == 0;
+    if (!conversion_failed) {
           // This will only work on Windows 10 and above.
-          auto result = SetThreadDescription(GetCurrentThread(), wstr.data());
-          if (FAILED(result)) {
-              std::error_code error_code(result, std::system_category());
-              throw std::system_error(error_code, "Failed to set thread description");
-          }
+      auto result = SetThreadDescription(GetCurrentThread(), wstr.data());
+      if (FAILED(result)) {
+        std::error_code error_code(result, std::system_category());
+        throw std::system_error(error_code, "Failed to set thread description");
       }
-   }
+    }
+  }
   if (conversion_failed) {
     std::error_code error_code_obj(GetLastError(), std::system_category());
     throw std::system_error(error_code_obj, "Failed to convert thread name to wide characters");
@@ -62,7 +62,7 @@ constexpr std::size_t MAXTHREADNAMESIZE = 16;
 
 void set_thread_name_posix(const std::string & name)
 {
-  std::string truncated_name; // At this scope to keep lifetime long enough
+  std::string truncated_name;  // At this scope to keep lifetime long enough
   const char * thread_name_ptr;
   // Truncate name to maximum length supported by pthread_setname_np
   // leaving one character for the null terminator
@@ -106,18 +106,20 @@ std::string get_thread_name()
     std::error_code error_code(result, std::system_category());
     throw std::system_error(error_code, "Failed to get thread name");
   }
-  int size_needed = WideCharToMultiByte(CP_UTF8, 0, thread_description, -1, nullptr, 0, nullptr, nullptr);
+  int size_needed = WideCharToMultiByte(CP_UTF8, 0, thread_description, -1, nullptr, 0, nullptr,
+      nullptr);
   bool conversion_failed = size_needed == 0;
   if (!conversion_failed) {
-      name = std::string(size_needed - 1, 0);
-      auto ret = WideCharToMultiByte(CP_UTF8, 0, thread_description, -1, &name[0], size_needed, nullptr, nullptr);
-      conversion_failed = ret == 0;
+    name = std::string(size_needed - 1, 0);
+    auto ret = WideCharToMultiByte(CP_UTF8, 0, thread_description, -1, &name[0], size_needed,
+        nullptr, nullptr);
+    conversion_failed = ret == 0;
   }
   LocalFree(thread_description);
   if (conversion_failed) {
-      DWORD error_code = GetLastError();
-      std::error_code error_code_obj(error_code, std::system_category());
-      throw std::system_error(error_code_obj, "Failed to convert thread name from wide characters");
+    DWORD error_code = GetLastError();
+    std::error_code error_code_obj(error_code, std::system_category());
+    throw std::system_error(error_code_obj, "Failed to convert thread name from wide characters");
   }
 #else  // posix and apple
   char thread_name[MAXTHREADNAMESIZE];  // This includes the null terminator
