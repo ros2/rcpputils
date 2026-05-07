4d549539fc8fad594682b1220bd355e027e9899d
mini-1235 <mauricepurnawan@gmail.com>
2026-05-07 11:09:09 +0000

    Lint
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- include/rcpputils/endian.hpp
+++ include/rcpputils/endian.hpp
@@ -25,7 +25,8 @@
 #define RCPPUTILS__ENDIAN_HPP_
 
 #if defined(_MSC_VER)
-    #pragma message("WARNING: rcpputils/endian.hpp is deprecated. Use std::endian from <bit> header instead.")
+    #pragma \
+  message("WARNING: rcpputils/endian.hpp is deprecated. Use std::endian from <bit> header instead.")
 #else
     #warning WARNING: rcpputils/endian.hpp is deprecated. Use std::endian from <bit> header instead.
 #endif
